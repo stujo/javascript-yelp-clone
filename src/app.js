cb5f46cd1065b1e31a68b90dd023d61409a298a5
Stujo <pair+stujo.yhordi@devbootcamp.com>
2016-07-06 12:16:16 -0700

    Add normalize.css

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { browserHistory, Router, Route, Redirect } from 'react-router'
 
 import 'font-awesome/css/font-awesome.css'
+import 'normalize.css/normalize.css'
 
 // Uses webpack css alias for styles
 import 'css/app.css'
