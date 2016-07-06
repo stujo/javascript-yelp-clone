27fc1000a0aec0d7c0aaad31c3dbf18f40edc5dc
Stujo <pair+stujo.yhordi@devbootcamp.com>
2016-07-06 16:23:37 -0700

    Add Google API usage

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -2,12 +2,47 @@ import React from 'react'
 
 import Map, { GoogleApiWrapper } from 'google-maps-react'
 
+import { searchNearby } from 'utils/googleApiHelpers'
+
 export class Container extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            places: [],
+            pagination: null
+        }
+    }
+
+    onReady(mapProps, map) {
+        const {google} = this.props;
+        const opts = {
+            location: map.center,
+            radius: '500',
+            types: ['cafe']
+        }
+        searchNearby(google, map, opts)
+            .then((results, pagination) => {
+                this.setState({
+                    places: results,
+                    pagination
+                })
+            }).catch((status, result) => {
+            // There was an error
+        })
+    }
+
     render() {
         return (
-            <div className="map">
+            <div>
               <h1>The Map</h1>
-              <Map className="map" google={ this.props.google } />
+              <Map google={ this.props.google } onReady={ this.onReady.bind(this) } visible={ false }>
+                { this.state.places.map(place => {
+                      return (<div key={ place.id }>
+                                { place.name }
+                              </div>)
+                  }) }
+              </Map>
             </div>
         )
     }
