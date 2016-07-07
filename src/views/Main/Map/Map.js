c49c225ccb49c760f39e1ec8770b3003d59b98b0
Stujo <pair+stujo.yhordi@devbootcamp.com>
2016-07-07 09:38:23 -0700

    Map on /map route

---
+++ src/views/Main/Map/Map.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './styles.module.css'
+
+class Map extends React.Component {
+
+    render() {
+        return (
+            <div className={ styles.map }>
+              Map!
+            </div>
+        )
+    }
+}
+
+module.exports = Map;
