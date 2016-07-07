f3d4bb22a6201f788941fd8981c4f4e5ce195932
Stujo <pair+stujo.yhordi@devbootcamp.com>
2016-07-07 16:12:05 -0700

    Google Map in Content Pane

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
+import { Map as GoogleMap } from 'google-maps-react'
+
 import styles from './styles.module.css'
 
 class Map extends React.Component {
 
     render() {
         return (
-            <div className={ styles.map }>
-              Map!
-            </div>
+            //    <div className={ styles.map }>hello</div>
+            <GoogleMap className={ styles.map } google={ this.props.google }>
+            </GoogleMap>
         )
     }
 }
 
 module.exports = Map;
+
+
+//            <GoogleMap className={ styles.map } google={ this.props.google }>
+//            </GoogleMap>
+
