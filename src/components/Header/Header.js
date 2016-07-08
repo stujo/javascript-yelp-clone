77655c2cfde2b9fd6db83ba2002f232a71229e11
Stujo <pair+stujo.yhordi@devbootcamp.com>
2016-07-08 13:56:59 -0700

    Header as pure stateless function

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,21 +4,19 @@ import styles from './styles.module.css'
 
 import { Link } from 'react-router'
 
-export class Header extends React.Component {
-    render() {
-        return (
-            <div className={ styles.topbar }>
-              <div className={ styles.brand }>
-                <Link to="/">
-                <h1>Yelp</h1>
-                </Link>
-              </div>
-              <section>
-                <Link to="/map">Map View</Link>
-              </section>
-            </div>
-        )
-    }
+const Header = (props) => {
+    return (
+        <div className={ styles.topbar }>
+          <div className={ styles.brand }>
+            <Link to="/">
+            <h1>Yelp</h1>
+            </Link>
+          </div>
+          <section>
+            <Link to="/map">Map</Link>
+          </section>
+        </div>
+    )
 }
 
 export default Header
